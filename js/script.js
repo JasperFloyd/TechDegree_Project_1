d9b88c763d193c55b0a79944d9d6a3615bf9b086
JasperFloyd <62623890+JasperFloyd@users.noreply.github.com>
2020-03-28 16:01:31 -0500

    fixing errors

--- js/script.js
+++ js/script.js
@@ -12,67 +12,75 @@ project 1 - A Random Quote Generator
 ***/
 var quotes = [
 {
-  quote: `'Never underestimate your ability to waste your own potential.'`, 
+  quote: 'Never underestimate your ability to waste your own potential.', 
   source: `Daniel Freeman`,
+  tag: 'HUMOR'
 },
 
 {
-  quote: `'Do or do not, there is no try.'`, 
+  quote: 'Do or do not, there is no try.', 
   source: 'Yoda', 
-  citation: 'Star Wars: The Empire Strikes Back',
-  time: 1466,
+  movie: 'Star Wars: The Empire Strikes Back',
+  year: 1980
 },
 
 {
   quote: 'Your own reason is the only oracle given you by heaven, and you are answerable for, not the rightness, but the uprightness of the decision.', 
-  source:'Thomas Jefferson'
+  source:'Thomas Jefferson',
+  tag: 'ILLUMINATION'
 },
 
 {
   quote: 'You got the gift, but it looks like youre waiting for something.', 
   source: 'The Oracle',
-  movie: 'The Matrix'
+  movie: 'The Matrix',
+  year: 1999
 },
 
 {
   quote: 'Illumination by the spirit is the endless end of every virtue.', 
-  source: 'Symeon the New Theologian'
+  source: 'Symeon the New Theologian',
+  tag: 'ILLUMINATION'
 },
 
 {
-  quote: 'You learn a lot about someone when you share a meal together', 
-  source: 'Anthony Bourdain'
+  quote: `Toto, I've got a feeling we're not in Kansas anymore`, 
+  source: 'Dorothy Gale',
+  movie: 'The Wizard of Oz',
+  year: 1939
 },
 
 {
-  quote: `'This above all, to thine own self be true.'`, 
+  quote: 'This above all, to thine own self be true.', 
   source:'Shakespeare', 
-  time: 487
+  tag: 'OPTIMIZE'
 },
 
 {
   quote: 'We aim above the mark to hit the mark.', 
-  source:'Ralph Waldo Emerson'
+  source:'Ralph Waldo Emerson',
+  tag: 'OPTIMIZE'
 },
 
 {
   quote: 'On some nights I still believe that a car with the gas needle on empty can run fifty more miles if you have the right music very loud on the radio', 
-  source: 'Hunter S. Thompson'
+  source: 'Hunter S. Thompson',
+  tag: 'MOTIVATION'
 },
 
 {
   quote: 'There is no balance, all or nothing', 
   source:'David Choe',
+  tag: 'TRUTH'
 }
 ];
 console.log(quotes);
 
 
-//this function will generate a random number between 0 and the length of the array
-let getRandomQuote = () => { 
+
+function getRandomQuote(){ 
   let ranNum;
-  ranNum = Math.ceil(Math.random() * quotes.length);
-  
+  ranNum = Math.floor(Math.random() * quotes.length);
   return quotes[ranNum];
 }
 
@@ -80,43 +88,42 @@ console.log(getRandomQuote());
 
 
 
-let randomColor = () => Math.floor(Math.random() * 999);
-let randomFinalColor = () => {
+function randomColor(){ 
+   num = Math.floor(Math.random() * 200);
+    return num
+}
+function randomFinalColor (){
   let colorR = randomColor();
   let colorG = randomColor();
   let colorB = randomColor();
   let mixColor = `rgb(${colorR},${colorG},${colorB})`;
   return document.body.style.setProperty('background-color', mixColor);
 }
-/***
- * `printQuote` function
-***/
+
+
+
 var screenDisplay;
-let printQuote = () => {
+function printQuote() {
   let currentQuote = getRandomQuote();
-    screenDisplay = `<p class="quote"> ${currentQuote.quote}</p>`
-                     `<p class="source"> ${currentQuote.source}`;                  
-            if(currentQuote.time){
-              screenDisplay +=  `<span class="time"> ${currentQuote.time}</span>`;
-              
-              if(currentQuote.chapter){
-                screenDisplay += `<span class="chapter"> ${currentQuote.chapter}</span>`;
-              }
-              if(currentQuote.movie){
-                screenDisplay += `<span class="movie"> Movie:  ${currentQuote.movie}</p>`;
-              }
-            }
+  screenDisplay = `<p class="quote"> ${currentQuote['quote']}</p>
+                    <p class="source"> ${currentQuote['source']}`;                  
+                        if(currentQuote.movie){
+                          screenDisplay += `<p class="movie">   ${currentQuote['movie']}</p>`;
+                        }
+                        if(currentQuote.year){
+                        screenDisplay +=  `<p class="time">  ${currentQuote['year']}\n</p>`;
+                        }
+                        if(currentQuote.tag){
+                        screenDisplay += `<p class="tag">${currentQuote['tag']}</p>`;
+                        }
             screenDisplay += `</p>`; 
             randomFinalColor();
             return document.getElementById('quote-box').innerHTML = screenDisplay;
   }
-  console.log(printQuote());
+  
 
 let timer = setInterval(printQuote,15000);
 
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
